1a9432b2597fa7e4bbbb3c04979021a6b21db42f
Tails developers <tails@boum.org>
2015-02-21 17:10:03 +0100

    Install i18n

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
-from distutils.core import setup
+#
+# Tor Monitor - a GTK applicaton to display Tor circuits and streams
+# Copyright (C) 2015  Tails developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup, Command
+from DistUtilsExtra.command import *
 
 setup(name='tormonitor',
       version='0.1',
+      description='a GTK applicaton to display Tor circuits and streams',
+      author='Tails developers',
+      author_email='tails@boum.org',
+      license='GNU GPL v3',
       scripts=['tormonitor'],
       data_files=[('share/icons/hicolor/scalable/apps', ['tormonitor.svg']),
                   ('share/applications', ['tormonitor.desktop'])],
-      requires=['stem','gi']
+      requires=['stem', 'gi'],
+      cmdclass = { "build" : build_extra.build_extra,
+                   "build_i18n" :  build_i18n.build_i18n,
+                   "build_help" :  build_help.build_help,
+                   "build_icons" :  build_icons.build_icons,
+                   "clean": clean_i18n.clean_i18n,
+                 }
      )
